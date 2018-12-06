7b226ab4b4a40765856c53d36ea9fcce9b623676
Erik Nordmark <eriknordmark@Eriks-MacBook-Pro.local>
2018-12-06 16:25:22 +0100

    Prod fix

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import app from "./index";
 const PORT = 8080;
 
-app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
-})
+// app.listen(PORT, () => {
+//     console.log('Express server listening on port ' + PORT);
+// })
+
+app.listen(PORT, '0.0.0.0', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  
+    console.info('>>> 🌎 Open http://0.0.0.0:%s/ in your browser.', PORT);
+  });
